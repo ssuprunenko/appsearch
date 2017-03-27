0ad0458feab4fc72e51c204d667e461f142c01d2
Sergey Suprunenko <suprunenko.s@gmail.com>
2017-03-27 21:19:30 +0200

    Use Timber.io

--- apps/api/config/config.exs
+++ apps/api/config/config.exs
@@ -28,3 +28,6 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
+
+# Import Timber, structured logging
+import_config "timber.exs"
--- apps/api/mix.exs
+++ apps/api/mix.exs
@@ -19,7 +19,7 @@ defmodule API.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:cowboy, :plug, :logger],
+    [extra_applications: [:cowboy, :plug, :logger, :timber],
      mod: {API.Application, []}]
   end
 
@@ -41,7 +41,8 @@ defmodule API.Mixfile do
       {:cowboy, "~> 1.1"},
       {:plug, "~> 1.3"},
       {:poison, "~> 3.1"},
-      {:app_store, in_umbrella: true}
+      {:timber, "~> 1.0"},
+      {:app_store, in_umbrella: true},
     ]
   end
 end
