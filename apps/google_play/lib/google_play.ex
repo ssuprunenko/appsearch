1a95127e049579a8afe7112c86a7367c06d273df
Sergey Suprunenko <suprunenko.s@gmail.com>
2017-03-27 20:59:55 +0200

    Initial Google Play commit

---
+++ apps/google_play/lib/google_play.ex
@@ -0,0 +1,18 @@
+defmodule GooglePlay do
+  @moduledoc """
+  Documentation for GooglePlay.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GooglePlay.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/google_play/test/google_play_test.exs
@@ -0,0 +1,8 @@
+defmodule GooglePlayTest do
+  use ExUnit.Case
+  doctest GooglePlay
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
