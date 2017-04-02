570cf23e5032782fcc47582afa028f141727ba52
Sergey Suprunenko <suprunenko.s@gmail.com>
2017-04-02 17:30:35 +0200

    Add /search endpoint

--- apps/api/lib/api.ex
+++ apps/api/lib/api.ex
@@ -16,6 +16,19 @@ defmodule API do
     |> send_file(200, Path.expand("..", __DIR__) <> "/public/index.html")
   end
 
+  get "/search" do
+    conn = fetch_query_params(conn)
+
+    appstore_apps = AppStore.search(conn.params["term"], conn.params)
+    googleplay_apps = GooglePlay.search(conn.params["term"], conn.params)
+    apps = appstore_apps ++ googleplay_apps
+
+    conn
+    |> put_resp_content_type("application/json")
+    |> put_resp_header("cache-control", "public, max-age=86400")
+    |> send_resp(200, Poison.encode!(apps, fields: conn.params["fields"]))
+  end
+
   get "/itunes/search" do
     conn = fetch_query_params(conn)
     apps = AppStore.search(conn.params["term"], conn.params)
