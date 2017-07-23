dc451441fa18d2e38b4eca169d410de5e52bab63
Sergey Suprunenko <suprunenko.s@gmail.com>
2017-07-23 19:59:43 +0200

    Return empty array when query is blank

--- apps/api/lib/api/apps.ex
+++ apps/api/lib/api/apps.ex
@@ -1,7 +1,8 @@
 defmodule API.Apps do
   @moduledoc false
 
-  def search(term, params \\ %{}) do
+  def search("", _params), do: []
+  def search(term, params) do
     appstore_task = Task.async(fn -> AppStore.search(term, params) end)
     googleplay_task = Task.async(fn -> GooglePlay.search(term, params) end)
 
